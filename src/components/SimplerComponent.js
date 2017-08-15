50fa677b458d3586d89e556b2f231845fb358689
yfern328 <yfernan2@u.rochester.edu>
2017-08-15 14:56:51 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -5,7 +5,7 @@ import React from 'react';
 const SimplerComponent = ({ handleClick }) => {
   return(
     <div onClick={ handleClick }>
-      'I am just happy'
+      "I am just happy"
     </div>
   );
 };
