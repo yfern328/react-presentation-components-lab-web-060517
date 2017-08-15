46c042a590d26329e733f2ab3b858e5e48f7fb30
yfern328 <yfernan2@u.rochester.edu>
2017-08-15 14:54:08 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
 // Code SimpleComponentHere Here
+
+import React from 'react';
+
+class SimpleComponent extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      mood: 'happy'
+    }
+  }
+
+  toggleMood = (event) => {
+    this.setState({
+      mood: 'sad'
+    })
+  }
+
+  render(){
+    return(
+      <div onClick={this.toggleMood}>
+        {this.state.mood}
+      </div>
+    );
+  }
+};
+
+export default SimpleComponent;
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,13 @@
 // Code SimplerComponentHere Here
+
+import React from 'react';
+
+const SimplerComponent = ({ handleClick }) => {
+  return(
+    <div onClick={ handleClick }>
+      'I am just happy'
+    </div>
+  );
+};
+
+export default SimplerComponent;
